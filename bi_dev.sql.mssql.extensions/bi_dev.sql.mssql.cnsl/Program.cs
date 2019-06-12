63020836013532bba2bfdb55c728ea78b0627ced
Алексей Шамшур <a.shamshur@tnomer.ru>
2019-06-12 12:15:19 +0300

    added web to utls

--- bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.cnsl/Program.cs
+++ bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.cnsl/Program.cs
@@ -1,4 +1,4 @@
-﻿using bi_dev.sql.mssql.extensions.@string;
+﻿using bi_dev.sql.mssql.extensions.web;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +12,7 @@ namespace bi_dev.sql.mssql.cnsl
         
         static void Main(string[] args)
         {
-            var e = Utils.UrlDecode("v2%7C%7C6189514023%7C%7C14186394606%7C%7Csmartprice%7C%7C1%7C%7Cpremium%7C%7Cnone%7C%7Csearch%7C%7Cno__v3%7C%7C6189514023%7C%7C14186394606%7C%7Csmartprice%7C%7C1%7C%7Cpremium%7C%7Cnone%7C%7Csearch%7C%7Cno", false);
-            var kaka = Utils.SplitString("opop: 1: 2: 3", ":", 3, true);
-            var kaka2 = Utils.SplitString("opop: 1: 2: 3", ":", 44, true);
-            var kaka3 = Utils.SplitString("", "", 44, true);
+            
         }
     }
 }
---
+++ bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions.string/bi_dev.sql.mssql.extensions.web/Utils.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace bi_dev.sql.mssql.extensions.web
+{
+    public static class Utils
+    {
+        [SqlFunction]
+        public static string Get(string url, string headersInUrlFormat, bool nullWhenError)
+        {
+            try
+            {
+                WebClient wc = new WebClient();
+                if (!string.IsNullOrWhiteSpace(headersInUrlFormat)) wc.Headers.Add(HttpUtility.ParseQueryString(headersInUrlFormat));
+                return wc.DownloadString(url);
+            }
+            catch (Exception e)
+            {
+                return Common.ThrowIfNeeded<string>(e, nullWhenError);
+            }
+        }
+        [SqlFunction]
+        public static string Post(string url, string body,  string headersInUrlFormat, bool nullWhenError)
+        {
+            try
+            {
+                WebClient wc = new WebClient();
+                if (!string.IsNullOrWhiteSpace(headersInUrlFormat)) wc.Headers.Add(HttpUtility.ParseQueryString(headersInUrlFormat));
+                return wc.UploadString(url, body);
+            }
+            catch (Exception e)
+            {
+                return Common.ThrowIfNeeded<string>(e, nullWhenError);
+            }
+        }
+    }
+}
