978f4aa977b240f0b1a6245ccefddb90d47ccaac
Алексей Шамшур <a.shamshur@tnomer.ru>
2019-08-03 13:26:19 +0300

    fix

--- bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions.web/Utils.cs
+++ bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions.web/Utils.cs
@@ -211,6 +211,7 @@ namespace bi_dev.sql.mssql.extensions.web
                 l.Add(new TableType("content_type", contentType));
                 l.Add(new TableType("code_page", res.CodePage.ToString()));
                 l.Add(new TableType("status_code", res.StatusCode.ToString()));
+                l.Add(new TableType("response_text", res.ResponseText));
                 if (res.RequestCookies != null)
                 {
                     foreach (var cookie in res.RequestCookies)
