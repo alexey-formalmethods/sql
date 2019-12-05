a44073a95037aef2f5043d551800742422af448f
Алексей Шамшур <a.shamshur@tnomer.ru>
2019-12-05 13:04:28 +0300

    added file

---
+++ bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions.file/Utils.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bi_dev.sql.mssql.extensions.file
+{
+    public class Utils
+    {
+        [SqlFunction]
+        public string GetFileContent(string fileName, bool nullWhenError)
+        {
+            try
+            {
+                return File.ReadAllText(fileName);
+            }
+            catch (Exception e)
+            {
+                return Common.ThrowIfNeeded<string>(e, nullWhenError);
+            }
+        }
+    }
+}
