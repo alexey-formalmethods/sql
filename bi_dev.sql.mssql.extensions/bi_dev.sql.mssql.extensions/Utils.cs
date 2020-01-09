6fbc8b4c7140bef964ec02e20eb1feea6ce7b221
Алексей Шамшур <a.shamshur@tnomer.ru>
2020-01-10 01:31:33 +0300

    added standart utils

---
+++ bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions/Utils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bi_dev.sql.mssql.extensions
+{
+    public static class Utils
+    {
+
+    }
+}
