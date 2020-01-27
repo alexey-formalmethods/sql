f601b6ec9c86e750406e2cfcd70a4cb740a14fe2
Алексей Шамшур <a.shamshur@tnomer.ru>
2020-01-27 14:34:50 +0300

    fix

--- bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions.aggregation/Utils.cs
+++ bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions.aggregation/Utils.cs
@@ -7,81 +7,79 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace bi_dev.sql.mssql.extensions.aggregation
+namespace bi_dev.sql.mssql.extensions.aggregation.Utils
 {
-    public class Utils
+    [Serializable]
+    [SqlUserDefinedAggregate(
+        Format.UserDefined, //use clr serialization to serialize the intermediate result  
+        IsInvariantToNulls = true, //optimizer property  
+        IsInvariantToDuplicates = false, //optimizer property  
+        IsInvariantToOrder = false, //optimizer property  
+        MaxByteSize = 8000
+    )] //maximum size in bytes of persisted value  
+    public class StringAgg: IBinarySerialize
     {
-        [Serializable]
-        [SqlUserDefinedAggregate(
-            Format.UserDefined, //use clr serialization to serialize the intermediate result  
-            IsInvariantToNulls = true, //optimizer property  
-            IsInvariantToDuplicates = false, //optimizer property  
-            IsInvariantToOrder = false, //optimizer property  
-            MaxByteSize = 8000
-        )] //maximum size in bytes of persisted value  
-        public class StringAgg: IBinarySerialize
+        /// <summary>  
+        /// The variable that holds the intermediate result of the concatenation  
+        /// </summary>  
+        public StringBuilder intermediateResult;
+
+        /// <summary>  
+        /// Initialize the internal data structures  
+        /// </summary>  
+        public void Init()
         {
-            /// <summary>  
-            /// The variable that holds the intermediate result of the concatenation  
-            /// </summary>  
-            public StringBuilder intermediateResult;
+            this.intermediateResult = new StringBuilder();
+        }
 
-            /// <summary>  
-            /// Initialize the internal data structures  
-            /// </summary>  
-            public void Init()
+        /// <summary>  
+        /// Accumulate the next value, not if the value is null  
+        /// </summary>  
+        /// <param name="value"></param>  
+        public void Accumulate(SqlString value, SqlString delimiter)
+        {
+            if (value.IsNull)
             {
-                this.intermediateResult = new StringBuilder();
+                return;
+            }
+            this.intermediateResult.Append(value.Value).Append(delimiter);
+        }
+
+        /// <summary>  
+        /// Merge the partially computed aggregate with this aggregate.  
+        /// </summary>  
+        /// <param name="other"></param>  
+        public void Merge(StringAgg other)
+        {
+            this.intermediateResult.Append(other.intermediateResult);
+        }
+
+        /// <summary>  
+        /// Called at the end of aggregation, to return the results of the aggregation.  
+        /// </summary>  
+        /// <returns></returns>  
+        public SqlString Terminate()
+        {
+            string output = string.Empty;
+            //delete the trailing comma, if any  
+            if (this.intermediateResult != null
+                && this.intermediateResult.Length > 0)
+            {
+                output = this.intermediateResult.ToString(0, this.intermediateResult.Length - 1);
             }
 
-            /// <summary>  
-            /// Accumulate the next value, not if the value is null  
-            /// </summary>  
-            /// <param name="value"></param>  
-            public void Accumulate(SqlString value, SqlString delimiter)
-            {
-                if (value.IsNull)
-                {
-                    return;
-                }
-                this.intermediateResult.Append(value.Value).Append(delimiter);
-            }
+            return new SqlString(output);
+        }
 
-            /// <summary>  
-            /// Merge the partially computed aggregate with this aggregate.  
-            /// </summary>  
-            /// <param name="other"></param>  
-            public void Merge(StringAgg other)
-            {
-                this.intermediateResult.Append(other.intermediateResult);
-            }
+        public void Read(BinaryReader r)
+        {
+            intermediateResult = new StringBuilder(r.ReadString());
+        }
 
-            /// <summary>  
-            /// Called at the end of aggregation, to return the results of the aggregation.  
-            /// </summary>  
-            /// <returns></returns>  
-            public SqlString Terminate()
-            {
-                string output = string.Empty;
-                //delete the trailing comma, if any  
-                if (this.intermediateResult != null
-                    && this.intermediateResult.Length > 0)
-                {
-                    output = this.intermediateResult.ToString(0, this.intermediateResult.Length - 1);
-                }
-
-                return new SqlString(output);
-            }
-
-            public void Read(BinaryReader r)
-            {
-                intermediateResult = new StringBuilder(r.ReadString());
-            }
-
-            public void Write(BinaryWriter w)
-            {
-                w.Write(this.intermediateResult.ToString());
-            }
+        public void Write(BinaryWriter w)
+        {
+            w.Write(this.intermediateResult.ToString());
         }
     }
+    
 }
