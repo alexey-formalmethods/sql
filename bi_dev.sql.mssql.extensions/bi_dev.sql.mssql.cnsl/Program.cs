be1204f6c3fff0d05804f0347cad688092baa006
Алексей Шамшур <a.shamshur@tnomer.ru>
2020-03-16 21:57:43 +0300

    add UTC times local/nonlocal

--- bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.cnsl/Program.cs
+++ bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.cnsl/Program.cs
@@ -1,4 +1,4 @@
-﻿using bi_dev.sql.mssql.extensions.@string.csv;
+﻿using bi_dev.sql.mssql.extensions.date;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions.date/Utils.cs
+++ bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions.date/Utils.cs
@@ -8,7 +8,7 @@ namespace bi_dev.sql.mssql.extensions.date
 {
     public static class Utils
     {
-        public static long? DateToUnixTimestamp(DateTime? value, bool includeMiliSeconds, bool nullWhenError)
+        public static long? LocalDateToUnixTimestamp(DateTime? value, bool includeMiliSeconds, bool nullWhenError)
         {
             try
             {
@@ -24,6 +24,23 @@ namespace bi_dev.sql.mssql.extensions.date
                 return Common.ThrowIfNeeded<long?>(e, nullWhenError);
             }
         }
+        public static long? DateToUnixTimestamp(DateTime? value, bool includeMiliSeconds, bool nullWhenError)
+        {
+            try
+            {
+                if (!value.HasValue) return null;
+                else
+                {
+                    DateTime? valueWithoutTimeZone = value.Value.ToUniversalTime() + new DateTimeOffset(value.Value).Offset;
+                    return LocalDateToUnixTimestamp(valueWithoutTimeZone, includeMiliSeconds, nullWhenError);
+                }
+            }
+            catch (Exception e)
+            {
+                return Common.ThrowIfNeeded<long?>(e, nullWhenError);
+            }
+            
+        }
         public static DateTime? UnixTimestampToDate(long? value, bool includeMiliSeconds, bool nullWhenError)
         {
             try
@@ -35,6 +52,10 @@ namespace bi_dev.sql.mssql.extensions.date
             {
                 return Common.ThrowIfNeeded<DateTime?>(e, nullWhenError);
             }
+        }
+        public static DateTime? UnixTimestampToLocalDate(long? value, bool includeMiliSeconds, bool nullWhenError)
+        {
+            return UnixTimestampToDate(value, includeMiliSeconds, nullWhenError)?.ToLocalTime();
         }
         public static int? GetWeekDayNumRus(DateTime? value, bool nullWhenError)
         {
