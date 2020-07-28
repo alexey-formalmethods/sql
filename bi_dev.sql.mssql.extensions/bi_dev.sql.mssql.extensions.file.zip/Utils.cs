f7f515bb1f3b8f37c879946c1420ea5b3129b65f
Алексей Шамшур <a.shamshur@tnomer.ru>
2020-07-29 01:49:02 +0300

    add file.zip

---
+++ bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions.file.zip/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+using Microsoft.SqlServer.Server;
+
+namespace bi_dev.sql.mssql.extensions.file.zip
+{
+    public static class Utils
+    {
+        [SqlFunction]
+        public static bool CreateZipFromDirectory(string sourceDirectoryName, string destinationArchiveFileName, bool falseWhenError)
+        {
+            try
+            {
+                ZipFile.CreateFromDirectory(sourceDirectoryName, destinationArchiveFileName);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return Common.ThrowIfNeeded<bool>(e, falseWhenError, false);
+            }
+            
+            
+        }
+    }
+}
