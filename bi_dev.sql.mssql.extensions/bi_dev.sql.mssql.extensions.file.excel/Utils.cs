e1d131de47a3d3395a19b968f909595fbd8191d4
Алексей Шамшур <a.shamshur@tnomer.ru>
2020-09-14 16:02:06 +0300

    excel get only string values

--- bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions.file.excel/Utils.cs
+++ bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions.file.excel/Utils.cs
@@ -378,7 +378,7 @@ namespace bi_dev.sql.mssql.extensions.file.excel
                                     cell.SetCellType(CellType.String);
                                     DataFormatter fmt = new DataFormatter();
                                     fmt.FormatCellValue(cell);
-                                    cellValue = cell.RichStringCellValue;
+                                    cellValue = cell.StringCellValue;
                                 }
                                 else
                                 {
