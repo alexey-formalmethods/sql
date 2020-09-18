58fae944b4b8a57f02f57e3292caf265ef0f19ed
Алексей Шамшур <a.shamshur@tnomer.ru>
2020-09-18 11:49:08 +0300

    add WaitFor

--- bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions/Utils.cs
+++ bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions/Utils.cs
@@ -8,6 +8,10 @@ namespace bi_dev.sql.mssql.extensions
 {
     public static class Utils
     {
-
+        public static bool WaitFor(int millisecondsDelay)
+        {
+            System.Threading.Tasks.Task.Delay(millisecondsDelay);
+            return true;
+        }
     }
 }
