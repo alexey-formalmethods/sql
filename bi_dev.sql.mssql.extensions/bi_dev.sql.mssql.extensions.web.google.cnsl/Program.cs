c7d97fd63afab8875284242b5126f9519e015b3f
Алексей Шамшур <a.shamshur@tnomer.ru>
2021-03-01 10:50:36 +0300

    fx

--- bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions.web.google.cnsl/Program.cs
+++ bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions.web.google.cnsl/Program.cs
@@ -14,12 +14,7 @@ namespace bi_dev.sql.mssql.extensions.web.google.cnsl
     {
         static void Main(string[] args)
         {
-            var e = Utils.GetRange(
-                "ya29.A0AfH6SMAjxEVBGbDwVD-1NVtAt4rLyv6TJNgYkg8WmJOWSSRC8TuSwz8z1x5Xvarbka0LIZ4GOv6fxcVVGXagrNy8SrVJ-rWPzYCkTwfWI4PDxxRZmxLuHgl1R1RGH9KmUFeRm2LXhMA1DGP_fWfZRbdgCdUm-Q",
-                "1hsaZwTwSpF0P4qrwnY3-e5jdqWvJcS0sAKd1FO-k5mc",
-                "tst",
-                null,
-                false);
+            
             
         }
        
