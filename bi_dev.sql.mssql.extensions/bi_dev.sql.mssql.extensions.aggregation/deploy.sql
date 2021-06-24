de3ebdd966350cefd05b1a483ccea1538e6cc08c
Алексей Шамшур <a.shamshur@tnomer.ru>
2021-06-24 15:08:29 +0300

    correct deploy.sql

--- bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions.aggregation/deploy.sql
+++ bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions.aggregation/deploy.sql
@@ -4,11 +4,10 @@ declare @build_location nvarchar(max) = N'C:\storage\ssd01\app\sql\bi_dev.sql.ms
 -------------------------------
 	declare @build_file_name nvarchar(4000) = @build_location + N'\bi_dev.sql.mssql.extensions.aggregation\bin\Debug\bi_dev.sql.mssql.extensions.aggregation.dll';
 -- drop existing --------
-	if (object_id('.dbo.f_clr_agg_median') is not null) drop function dbo.f_clr_agg_median;
+	if (object_id('.dbo.f_clr_agg_median') is not null) drop aggregate dbo.f_clr_agg_median;
 	
 	if (exists (select 1 from sys.assemblies where name = N'mssql.extensions.aggregation')) drop assembly [mssql.extensions.aggregation];
 -- create new --------------------
 	create assembly [mssql.extensions.aggregation] from @build_file_name with permission_set = unsafe;
 	go
-	create aggregate dbo.f_clr_agg_median(@value float, @is_null_equal_to_zero bit) returns float external name [mssql.extensions.aggregation].[Median];
-	
+	create aggregate dbo.f_clr_agg_median(@value float, @is_null_equal_to_zero bit) returns float external name [mssql.extensions.aggregation].[bi_dev.sql.mssql.extensions.aggregation.Utils.Median];
