d65e0f6abf9cea5ab34b9eafe32e7ffa54573d7f
Алексей Шамшур <a.shamshur@tnomer.ru>
2021-06-30 13:12:45 +0300

    remove same names

--- bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions.string.csv/Utils.cs
+++ bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions.string.csv/Utils.cs
@@ -109,7 +109,7 @@ namespace bi_dev.sql.mssql.extensions.@string.csv
                 return Common.ThrowIfNeeded<string>(e, nullWhenError);
             }
         }
-        private static CsvReadResult GetCsvContent(string value, string delimiter, bool isFirstRowWithColumnNames)
+        private static CsvReadResult getCsvContent(string value, string delimiter, bool isFirstRowWithColumnNames)
         {
             var result = parseCsv(value, delimiter);
             CsvReadResult csvResult = new CsvReadResult();
