f1f6e6c2f1b0fdaa08d29328940b6b6c79425e95
Алексей Шамшур <a.shamshur@tnomer.ru>
2021-08-02 13:13:58 +0300

    added 2 deploy scripts

--- bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions.string.csv/deploy.sql
+++ bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions.string.csv/deploy.sql
@@ -11,7 +11,7 @@ declare @build_location nvarchar(max) = N'C:\storage\ssd01\app\sql\bi_dev.sql.ms
 -- create new --------------------
 	create assembly [mssql.extensions.string.csv] from @build_file_name with permission_set = unsafe;
 	go
-	create function dbo.f_clr_csv_get_content(@value nvarchar(max), @delimiter nvarchar(max), @is_first_row_with_column_names bit, @null_when_error bit) returns nvarchar(max) with execute as owner as external name [mssql.extensions.string.csv].[bi_dev.sql.mssql.extensions.string.csv.Utils].GetCsvContent;
+	create function dbo.	(@value nvarchar(max), @delimiter nvarchar(max), @is_first_row_with_column_names bit, @null_when_error bit) returns nvarchar(max) with execute as owner as external name [mssql.extensions.string.csv].[bi_dev.sql.mssql.extensions.string.csv.Utils].GetCsvContent;
 	go
 	create function dbo.f_clr_parse_csv(@value nvarchar(max), @delimiter nvarchar(max), @null_when_error bit) 
 	returns table([row_number] int, column_index int, row_value nvarchar(max))
