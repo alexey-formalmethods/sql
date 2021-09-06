b5574cf4440e83917bc55b2b9497254ce4404da1
Алексей Шамшур <a.shamshur@tnomer.ru>
2021-09-07 00:09:06 +0300

    add value tuple to csv lib

--- bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions.string.csv/deploy.sql
+++ bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions.string.csv/deploy.sql
@@ -1,3 +1,19 @@
+-- System.ValueTuple--------
+declare @system_value_tuple_location nvarchar(4000) = 'C:\Windows\Microsoft.NET\Framework64\v4.0.30319\System.ValueTuple.dll';
+if (not exists (select 1 from sys.assemblies where name = N'System.ValueTuple')) create ASSEMBLY [System.ValueTuple] from @system_value_tuple_location with permission_set = unsafe;
+else begin
+	begin try
+		alter ASSEMBLY [System.ValueTuple] from @system_value_tuple_location with permission_set = unsafe;
+	end try
+	begin catch
+		declare @error_message nvarchar(max) = error_message();
+		if (@error_message not like '%identical to an assembly that is already registered under the name%') begin
+			raiserror(@error_message, 16, 1);
+		end
+	end catch
+end
+go
+----------------------------
 -- INPUT --
 -- determin project-location
 declare @build_location nvarchar(max) = N'C:\storage\ssd01\app\sql\bi_dev.sql.mssql.extensions';
@@ -11,7 +27,7 @@ declare @build_location nvarchar(max) = N'C:\storage\ssd01\app\sql\bi_dev.sql.ms
 -- create new --------------------
 	create assembly [mssql.extensions.string.csv] from @build_file_name with permission_set = unsafe;
 	go
-	create function dbo.	(@value nvarchar(max), @delimiter nvarchar(max), @is_first_row_with_column_names bit, @null_when_error bit) returns nvarchar(max) with execute as owner as external name [mssql.extensions.string.csv].[bi_dev.sql.mssql.extensions.string.csv.Utils].GetCsvContent;
+	create function dbo.f_clr_csv_get_content(@value nvarchar(max), @delimiter nvarchar(max), @is_first_row_with_column_names bit, @null_when_error bit) returns nvarchar(max) with execute as owner as external name [mssql.extensions.string.csv].[bi_dev.sql.mssql.extensions.string.csv.Utils].GetCsvContent;
 	go
 	create function dbo.f_clr_parse_csv(@value nvarchar(max), @delimiter nvarchar(max), @null_when_error bit) 
 	returns table([row_number] int, column_index int, row_value nvarchar(max))
