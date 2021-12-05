d5002da3b95760b083755a43bbec325d1f8fe89e
Алексей Шамшур <a.shamshur@tnomer.ru>
2021-12-05 16:37:14 +0300

    remove cnsl

--- bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.cnsl/Program.cs
+++ bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.cnsl/Program.cs
@@ -6,7 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using bi_dev.sql.mssql.extensions.web2;
+using bi_dev.sql.mssql.extensions.file.excel;
 using Newtonsoft.Json;
 
 namespace bi_dev.sql.mssql.cnsl
@@ -15,8 +15,16 @@ namespace bi_dev.sql.mssql.cnsl
     {
         public static void Main()
         {
-            var body = "{ \"url\":\"https://tradernet.ru/portfolios/ajax-get-trades\",\"method\":\"POST\",\"body\":\"skip=0&take=1000&pageSize=500&sort[0][field]=date&sort[0][dir]=desc\",\"headers\":[{ \"name\":\"Content-Type\",\"value\":\"application/x-www-form-urlencoded; charset=UTF-8\"}],\"cookies\":[{ \"name\":\"SID\",\"value\":\"l3kl8gid16oshtjhulm032c4vk\"}]}";
-            Utils.WebProcessRequest(body, true);
-    }
+            Utils.GetExcelContent(
+                @"C:\a.shamshur\Robocash - for publication.xlsx",
+                "2021_08_04_Debex_list",
+                null,
+                null,
+                null,
+                null,
+                true,
+                false
+            );
+        }
     }
 }
