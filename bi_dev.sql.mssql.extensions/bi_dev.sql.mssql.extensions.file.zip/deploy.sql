e44a568996544d212444792ce3a3d5f74afb5d3e
Алексей Шамшур <a.shamshur@tnomer.ru>
2022-02-25 15:50:23 +0300

    zip change clr func name

--- bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions.file.zip/deploy.sql
+++ bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions.file.zip/deploy.sql
@@ -32,10 +32,10 @@ end
 	declare @build_location nvarchar(max) = N'C:\storage\hdd01\app\sql\bi_dev.sql.mssql.extensions';
 	declare @build_file_name nvarchar(4000) = @build_location + N'\bi_dev.sql.mssql.extensions.file.zip\bin\Debug\bi_dev.sql.mssql.extensions.file.zip.dll';
 -- drop existing --------
-	if (object_id('.dbo.f_clr_create_zip_from_directory') is not null) drop function dbo.f_clr_create_zip_from_directory;
+	if (object_id('.dbo.f_clr_zip_create_from_directory') is not null) drop function dbo.f_clr_zip_create_from_directory;
 	if (exists (select 1 from sys.assemblies where name = N'mssql.extensions.file.zip')) drop assembly [mssql.extensions.file.zip];
 -------------------------
 	create assembly [mssql.extensions.file.zip] from @build_file_name with permission_set = unsafe;
 	go
-	create function f_clr_create_zip_from_directory(@directory_name nvarchar(max), @destination_file_name nvarchar(max), @false_when_error bit) returns bit with execute as owner as external name [mssql.extensions.file.zip].[bi_dev.sql.mssql.extensions.file.zip.Utils].CreateZipFromDirectory;
+	create function f_clr_zip_create_from_directory(@directory_name nvarchar(max), @destination_file_name nvarchar(max), @false_when_error bit) returns bit with execute as owner as external name [mssql.extensions.file.zip].[bi_dev.sql.mssql.extensions.file.zip.Utils].CreateZipFromDirectory;
 	go
