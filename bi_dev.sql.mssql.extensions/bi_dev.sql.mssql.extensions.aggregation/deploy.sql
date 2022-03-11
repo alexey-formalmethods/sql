40837b4fbaf4ab07be88c14d2f6867f9a3b163b8
alexey-formalmethods <Suncircle2006@mail.ru>
2022-03-11 14:55:12 +0300

    Update deploy.sql

--- bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions.aggregation/deploy.sql
+++ bi_dev.sql.mssql.extensions/bi_dev.sql.mssql.extensions.aggregation/deploy.sql
@@ -1,6 +1,44 @@
--- INPUT --
--- determin project-location
-declare @build_location nvarchar(max) = N'C:\storage\ssd01\app\sql\bi_dev.sql.mssql.extensions';
+-- input variables -----------------------------
+	if (object_id('tempdb..#t_tmp_var') is not null) drop table #t_tmp_var;
+	create table #t_tmp_var (name nvarchar(max), value nvarchar(max));
+	insert into #t_tmp_var
+	(
+		name
+	  , value
+	)
+	select
+		 name, value
+	from (values
+		 ('@system_runtime_serialization_location', N'C:\Windows\Microsoft.NET\Framework64\v4.0.30319\System.Runtime.Serialization.dll')
+		,('@build_location', N'C:\storage\hdd01\proj\sql\bi_dev.sql.mssql.extensions')
+	) t (name, value);
+	go
+------ assembiles -------------
+------ System.Runtime.Serialization -------------
+	declare @system_runtime_serialization_location nvarchar(4000);
+	select
+		 @system_runtime_serialization_location = t.value
+	from #t_tmp_var t
+	where t.name = '@system_runtime_serialization_location';
+	if (not exists (select 1 from sys.assemblies where name = N'System.Runtime.Serialization')) create ASSEMBLY [System.Runtime.Serialization] from @system_runtime_serialization_location with permission_set = unsafe;
+	else begin
+		begin try
+			alter ASSEMBLY [System.Net.Http] from @system_runtime_serialization_location with permission_set = unsafe;
+		end try
+		begin catch
+			declare @error_message nvarchar(max) = error_message();
+			if (@error_message not like '%identical to an assembly that is already registered under the name%') begin
+				raiserror(@error_message, 16, 1);
+			end
+		end catch
+	end
+	go
+-- start ---------------------------
+	declare @build_location nvarchar(max);
+	select
+		 @build_location = t.value
+	from #t_tmp_var t
+	where t.name = '@build_location'
 -------------------------------
 	declare @build_file_name nvarchar(4000) = @build_location + N'\bi_dev.sql.mssql.extensions.aggregation\bin\Debug\bi_dev.sql.mssql.extensions.aggregation.dll';
 -- drop existing --------
